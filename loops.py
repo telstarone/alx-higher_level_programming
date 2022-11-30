2003f3fcd5cc1c180bea293f0a9ae44be0887bd9
telstarone <gvnr.ke@gmail.com>
2022-11-30 01:01:58 +0000

    loops

---
+++ loops.py
@@ -0,0 +1,41 @@
+if 12 == 48/3 or 12 == 12:
+    print("Holberton")
+else:
+    print("School")
+
+print("###")
+
+a = 12
+if a < 2:
+    print("Holberton")
+elif a % 2 == 0:
+    print("C is fun")
+else:
+    print("School")
+
+print("###")
+
+a = 12
+if a > 2:
+    if a % 2 == 0:
+        print("Holberton")
+    else:
+        print("C is fun")
+else:
+    print("School")
+
+print("###")
+
+if 12 == 48/4 and False:
+    print("Holberton")
+else:
+    print("School")
+
+print("###")
+
+if True:
+    print("Holberton")
+else:
+    print("School")
+
+print("###")
