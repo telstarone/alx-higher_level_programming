7abe3a83db9265d30e70d56257bc791f2bb0a234
telstarone <gvnr.ke@gmail.com>
2022-12-06 20:33:23 +0000

    corrected indentation

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,15 @@ def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
 
         print("")
+
+
+"""
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))
+"""
